de48c8f182e8d4a733da5797fad7ef0f2d01a13b
captainabap <github@brandeis.de>
2014-10-08 23:57:52 +0200

    c

--- js/helper.js
+++ js/helper.js
@@ -26,7 +26,7 @@ var HTMLlocation = "<li class='flex-item'><span class='orange-text'>location</sp
 var HTMLbioPic = "<img src='%data%' class='biopic'>";
 var HTMLWelcomeMsg = "<span class='welcome-message'>%data%</span>";
 
-var HTMLskillsStart = "<h3 id='skillsH3'>Skills at a Glance:</h3><ul id='skills' class='flex-box'></ul>";
+var HTMLskillsStart = "<h3 id='skills'>Skills at a Glance:</h3><ul id='skills' class='flex-box'></ul>";
 var HTMLskills = "<li class='flex-item'><span class='white-text'>%data%</span></li>";
 
 var HTMLworkStart = "<div class='work-entry'></div>";
@@ -58,6 +58,10 @@ var HTMLonlineURL = "<br><a href='#'>%data%</a>";
 var internationalizeButton = "<button>Internationalize</button>";
 var googleMap = "<div id='map'></div>";
 
+function inName(name){
+	name = name.trim().split(" ");
+return token[0] + " " + token[1].toUpperCase( );
+}
 
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,64 +1,100 @@
-var skills = [ 	"ABAP OO", 
+var work = { 
+	"jobs" : [ { 	"employer" 		: "zetVisions AG", 
+					"title" 		: "Head of ABAP development",
+					"dates" 		: "2002 - today",
+					"city" 			: "Heidelberg",
+					"description"	: "Fachliche Leitung der ABAP Entwicklung. Führung des Entwicklungsteams, Architektur der Anwendung, Definition und Optimierung des Entwicklungsprozesses, Fortbildung des Entwicklerteams," +  
+									"Zertifizierung der Produkte, Planung und Koordination der Entwicklungsinfrastruktur. Technischer Ansprechpartner für das Produktmanagement und Sales. " },
+			{ 		"employer" 		: "BIT Institut", 
+					"title" 		:  "HiWi",
+					"dates" 		:  "2000 - 2002",
+					"city" 			: "Mannheim",
+					"description"	: "Softwareevaluation, Usability Tests, Installation der Oracle Applications. Seminarleiter für das Seminar 'Customizing eines ERP Systems für den Mittelstand' "+
+									  "am Beispiel der Oracle SMB Suite. Analyse von SAP Business One " }]};
+	
+var skills = [	"ABAP OO", 
 				"WebDynpro ABAP", 
 				"TDD",
 				"Scrum", 
 				"Clean Code", 
 				"Java" ];
-				
+		
 var bio = { "name" : "Jörg Brandeis",
 			"role" : "Head of ABAP development",
-			"contactInfo" : { "mobile" : "#49-1775407426",
-							  "email"  : "joerg@brandeis.de",
-							  "street" : "Rheindammstrasse 5",
-							  "city"   : "Mannheim",
-							  "zip"    : "68163" } ,
+			"welcomeMessage" : "Welcome to my bio",
+			"contacts" : { "mobile" : "#49-1775407426",
+						   "email"  : "joerg@brandeis.de",
+						   "location" : "68163 Mannheim, Rheindammstrasse 5"} ,
 			"imageURL" : "https://x1.xingassets.com/img/users/0/0/7/9b36e0c5f.3262104,3.140x185.jpg",
-			"skills" : skills,
-			"welcomeMessage" : "Welcome to my bio" };
+			"skills" : skills };
 
 			
-var work = { "jobs" : [
- { "title" :  "Head of ABAP development",
-   "employer" : "zetVisions AG", 
-	"dates" :  "2002 - today",
-	"city" : "Heidelberg",
-	"description": "Fachliche Leitung der ABAP Entwicklung" } ]}
-	
-/* var education = { };
-education["school"] = "Universität Mannheim";
-education["duration"] = 5;
-education["city"] = "Mannheim";
-education["degree"] = "Diplom Wirtschaftsinformatik"; */
-
 var education = {
 	 "schools" :  [
-	 { "name" : "Neckarschule",
+		{ "name" : "Neckarschule",
 		"city" : "Heidelberg",
 		"degree" : "",
 		"majors" : ["Deutsch", "Mathe", "Sachkunde"]
 		},
-	{ "name" : "Hölderlin Gymnasium",
+		{ "name" : "Hölderlin Gymnasium",
 		"city" : "Heidelberg",
 		"degree" : "Abitur",
 		"majors" : ["Mathe", "Physik"]
-		},	 { "name" : "Universität Mannheim",
+		},	 
+		{ "name" : "Universität Mannheim",
 		"city" : "Mannheim",
 		"degree" : "Diplom Wirtschaftsinformatik",
 		"majors" : ["Wirtschaftsinformatik"]
-		}]
-}
-
-var onlineCourses = {
-	[ {"title" : "JavaScript Crash Course",
-		"school" : "Udacity",
-		"dates"  : 2014,
-		"url"    : "http://www.udacity.com"}
+		}],
+		
+	"course": [ {"title" : "JavaScript Crash Course",
+				"school" : "Udacity",
+				"dates"  : 2014,
+				"url"    : "http://www.udacity.com"}
+				,
+				{"title" : "iPhone / iPad Entwickler Seminar",
+				"school" : "Kress",
+				"dates"  : 2012,
+				"url"    : "http://www.objectiveceeds.com"}
 	]
 }
+
+
+var projects = {
+"project" : [ { "title" : "zetControl @ IBG"},{"title":"Daimler"}]
+}
+
+projects.display = function( ){
+		$("#projects").append( HTMLprojectStart);
+	for ( counter in projects.project ){
+		$("#projects").append( HTMLprojectTitle.replace("%data%",projects.project[counter]));
+	}
+}
+
+projects.display( );
+
+	function displayWork(){
+		$("#workExperience").append( HTMLworkStart );
+			for ( job in work.jobs){		
+				$("#workExperience").append( HTMLworkEmployer.replace( "%data%", work.jobs[job].employer ) + HTMLworkTitle.replace( "%data%", work.jobs[job].title  ));
+
+				$("#workExperience").append( HTMLworkDates.replace( "%data%", work.jobs[job].dates ));
+				$("#workExperience").append( HTMLworkLocation.replace( "%data%", work.jobs[job].city ));
+				$("#workExperience").append( HTMLworkDescription.replace( "%data%", work.jobs[job].description ));
+			}
+	}
+	displayWork( );
+	function logClicks( x, y ){
+	console.log( x);
+	}
 	
-/*		
-$("#header").prepend( HTMLmobile.replace( "%data%", bio.contactInfo.mobile ) );
-$("#header").prepend( HTMLemail.replace( "%data%", bio.contactInfo.email ) );
+	$(document).click(function(loc){
+		logClicks(loc.pageX, loc.pageY);
+	});
+	$("#mapDiv").append(googleMap);
+		
+$("#header").prepend( HTMLmobile.replace( "%data%", bio.contacts.mobile ) );
+$("#header").prepend( HTMLemail.replace( "%data%", bio.contacts.email ) );
 $("#header").prepend( HTMLheaderRole.replace( "%data%", bio.role ) );
 $("#header").prepend( HTMLheaderName.replace( "%data%", bio.name ) );
 $("#header").prepend( HTMLbioPic.replace( "%data%", bio.imageURL ) );
@@ -73,7 +109,7 @@ $("#workExperience").append( HTMLworkEmployer.replace( "%data%", work.employer )
 $("#workExperience").append( HTMLworkTitle.replace( "%data%", work.position ));
 $("#workExperience").append( HTMLworkLocation.replace( "%data%", work.city ));
 $("#workExperience").append( HTMLworkDescription.replace( "%data%", work.description ));
-
+/*
 var HTMLschoolStart = "<div class='education-entry'></div>";
 var HTMLschoolName = "<a href='#'>%data%";
 var HTMLschoolDegree = " -- %data%</a>";
